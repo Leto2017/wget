54bfd7450ada06e15cc9330ebdafb7f1f7d5eb93
Leto2017 <33726236+Leto2017@users.noreply.github.com>
2018-12-02 21:09:27 +0500

    Add files via upload
    reading arguments from command line added

--- main.cpp
+++ main.cpp
@@ -7,15 +7,50 @@
 #include <fstream>
 #include <vector>
 #include <cctype>
+#include <iostream>
 using namespace std;
 
 void getHost_byName(char* hostname, char* ip);
 int read(char* address, int socket_desc, char* hostname);
 string getFileName(char* hostname);
 
+static void show_usage(std::string name)
+{
+    std::cerr << "Usage: <option(s)> SOURCES"
+              << "Options:\n"
+              << "\t-h,--help\t\tShow this help message\n"
+              << "\t-d,--destination DESTINATION\tSpecify the destination path"
+              << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
-    char* hostname = "www.google.com";
+//    if (argc < 2) {
+//        show_usage(argv[0]);
+//        return 1;
+//    }
+
+    char* hostname;
+    std::vector <std::string> sources;
+    std::string destination;
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if ((arg == "-h") || (arg == "--help")) {
+            show_usage(argv[0]);
+            return 0;
+        }
+        else {hostname = argv[1];}
+        //else if ((arg == "-d") || (arg == "--destination")) {
+          //  if (i + 1 < argc) { // Make sure we aren't at the end of argv!
+          //      destination = argv[i++]; // Increment 'i' so we don't get the argument as the next argv[i].
+          //  } else { // Uh-oh, there was no argument to the destination option.
+          //        std::cerr << "--destination option requires one argument." << std::endl;
+          //      return 1;
+          //  }
+        //} else {
+        //    sources.push_back(argv[i]);
+       // }
+    }
 
     //create socket
     int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
@@ -56,7 +91,7 @@ int read(char* address, int socket_desc, char* hostname)
 	puts(hostname);
 
 	char* message;
-	const char * format = "GET \/ HTTP\/1.1\r\nHost: %s\r\nUser-Agent: fetch.c\r\n\r\n";
+	const char * format = "GET / HTTP/1.1\r\nHost: %s\r\nUser-Agent: fetch.c\r\n\r\n";
 	int status = asprintf(& message, format, hostname);
     if (status == -1)
     {
