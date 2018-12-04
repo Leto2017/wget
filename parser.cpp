196b6bf89330d85278fd5f546531c543b625f504
NDeyneka <45417642+NDeyneka@users.noreply.github.com>
2018-12-04 21:52:06 +0500

    Update parser.cpp
    Добавлена иерархия и работа с флагом no_parent

--- parser.cpp
+++ parser.cpp
@@ -42,94 +42,145 @@ void parse_text( vector<string> &link_list, string string_to_parse)
 void parse_html( vector<char*> &link_list, string filename, char* hostname, bool no_parent, int lvl)
 {
 	string tmp = "";
+	string buf = "";
+	string sub="";
+
 	char current[50];
 
 
 	bool addr=false;
 	//bool end=true;
-	//bool read = true;
+	bool res;
 
 	FILE *f;
 
 	char *pos;
+	int beg;
+	int end;
+	int size;
+
 	char c;
 
 	int cur_lvl=1;
 	int slash_cnt=0;
 
-	f = fopen(filename.c_str(), "r");
-
-	while(fgets(current, sizeof(current), f))
+	if(lvl!=1)
 	{
-		if(!addr) pos = strstr(current, "<a");
-		else pos = current;
+		f = fopen(filename.c_str(), "r");
 
-		if(pos!=NULL || addr)
+		while(fgets(current, sizeof(current), f))
 		{
-			int i=0;
-			//end = false;
+			if(!addr) pos = strstr(current, "<a");
+			else pos = current;
 
-			while(*(pos+i)!='>' && !(current+46==pos+i) && !addr)
+			if(pos!=NULL || addr)
 			{
-				if(*(pos+i)=='h' && *(pos+i+1)=='r' && *(pos+i+2)=='e' && *(pos+i+3)=='f')
-				{
-					i=i+4;
-					addr = true;
-					break;
-				}
-				else i++;
-			}
+				int i=0;
 
-			if(addr)
-			{
-				while((*(pos+i)!='>') && !(current+50==pos+i))
+				while(*(pos+i)!='>' && !(current+46==pos+i) && !addr)
 				{
-					if(*(pos+i)=='=' && *(pos+i+1)=='\"') i=i+2;
-					else 
+					if(*(pos+i)=='h' && *(pos+i+1)=='r' && *(pos+i+2)=='e' && *(pos+i+3)=='f')
 					{
-						if(*(pos+i)!='\"') 
+						i=i+4;
+						addr = true;
+						break;
+					}
+					else i++;
+				}
+
+				if(addr)
+				{
+					while((*(pos+i)!='>') && !(current+50==pos+i))
+					{
+						if(*(pos+i)=='=' && *(pos+i+1)=='\"') 
 						{
-							c = *(pos+i);
-							if(c!=0) tmp.push_back(c);
-
-							if(c=='/') slash_cnt++;
-							if(slash_cnt>=3) cur_lvl=slash_cnt-1;
+							i=i+2;
 						}
-						else break;
+						else 
+						{
+							if(*(pos+i)!='\"') 
+							{
+								c = *(pos+i);
+								if(c!=0) tmp.push_back(c);
 
-						i++;
+								if(c=='/') 
+								{
+									slash_cnt++;
+
+									if(slash_cnt==2) beg = tmp.length();
+									else if(slash_cnt==3) end = tmp.length()-1;
+								}
+								if(slash_cnt>=3) cur_lvl=slash_cnt-1;
+							}
+							else break;
+
+							i++;
+						}
 					}
-				}
 
-				if(current+50==pos+i) 
-				{
-					//end = true;
-					continue;
-				}
-				else
-				{
-					if((!tmp.substr(0,7).compare("http://") || !tmp.substr(0,8).compare("https://")) && lvl>1)
+					if(current+50==pos+i) 
 					{
-						
-						printf("%s\n", tmp.c_str());
-						link_list.push_back((char*)tmp.c_str());
-				
-						cur_lvl=1;
-						slash_cnt=0;
+						continue;
 					}
-					tmp="";
-					addr=false;
-					//end = false;
+					else
+					{
+						if((!tmp.substr(0,7).compare("http://") || !tmp.substr(0,8).compare("https://") || !tmp.substr(0,6).compare("ftp://")) && lvl>1)
+						{
+							if(no_parent)
+							{
+								buf = tmp.substr(beg, tmp.length());
+								size = string(hostname).length();
+								sub = buf.substr(0,size);
+								res = sub.compare((string)hostname);
+
+								if(!res)
+								{
+									printf("%s\n", tmp.c_str());
+									link_list.push_back((char*)tmp.c_str());
+								}
+						
+								cur_lvl=1;
+								slash_cnt=0;
+								buf="";
+								sub="";
+							}
+							else
+							{
+								buf = tmp.substr(beg, end-beg);
+								sub = (string)hostname;
+								size = buf.length();
+								res = buf.compare(sub.substr(0,size));
+
+								if(!res)
+								{
+									printf("%s\n", tmp.c_str());
+									link_list.push_back((char*)tmp.c_str());
+								}
+						
+								cur_lvl=1;
+								slash_cnt=0;
+								buf="";
+								sub="";
+							}
+						}
+						tmp="";
+						addr=false;
+						
+					}
+
 				}
 
 			}
+		}	
 
-		}
-	}	
+		fclose(f);
 
-	fclose(f);
-
-
+	}
+	else
+	{
+		printf("%s\n", hostname);
+		link_list.push_back(hostname);
+	}
 }
 
 int main()
